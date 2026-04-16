6eeeb75e7ee5fccf867c7347103c7062d7431135
Fernando Cunha Júnior <laurenticunha@gmail.com>
2026-04-16 15:45:47 -0300

    Testando envio

---
+++ Algoritmo_1.java
@@ -0,0 +1,18 @@
+public class Algoritmo_1 {
+    
+    // Realizar um algoritmo de instruções detalhadas de como realizar um café coado, utilizando System.out.println() para cada etapa do processo.
+
+    public static void main(String[] args) {
+
+        System.out.println("Passo a passo da preparação do café coado");
+        System.out.println("Aquecer a água (Temperatura ideal: 90-96°C)");
+        System.out.println("Coloque o filtro no coador e passe um pouco de água quente nele para retirar o gosto de papel");
+        System.out.println("Adicione o café (1 colher de sopa para cada 100ml de água)");
+        System.out.println("Comece a coar, coloque um pouco de água quente sobre o café para molhar, espere 30 segundos para liberar os aromas");
+        System.out.println("Continue despejando a água quente lentamente sobre o café, fazendo movimentos circulares");
+        System.out.println("Espere coar completamente, até parar de pingar");
+        System.out.println("Agora é só servir e aproveitar seu café coado!");
+        
+
+    }
+}
