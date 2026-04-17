43b5a5e0761b374cd760654ee1917efd31e8eb30
Fernando Cunha Júnior <laurenticunha@gmail.com>
2026-04-17 11:29:45 -0300

    Adicionando algoritmo de area do circulo

---
+++ aula2/Area_Circulo.java
@@ -0,0 +1,25 @@
+package aula2;
+
+import java.util.Scanner;
+
+public class Area_Circulo {
+    // Faça um algoritmo para calcular a área do círculo
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        double raio = 0;
+        double pi = 3.14;
+
+        System.out.println("Calculadora de área do círculo");
+
+        System.out.println("Qual é o raio do círculo: ");
+        raio = sc.nextDouble();
+
+        double area = pi * (raio * raio);
+
+        System.out.println("A área do círculo é: " + area);
+
+    }
+}
