18931065cd93e17b18b19dd61e17e7f6e287c207
Fernando Cunha Júnior <laurenticunha@gmail.com>
2026-04-17 14:11:10 -0300

    Enviando algoritmo que determina se o ano é bissexto

---
+++ aula3/Ano_Bissexto.java
@@ -0,0 +1,28 @@
+package aula3;
+
+import java.util.Scanner;
+
+public class Ano_Bissexto {
+    // Faça um algoritmo para determinar se um ano é bissexto
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int ano;
+
+        System.out.println("Determine se um ano é bissexto");
+
+        System.out.println("Digite um ano: ");
+        ano = sc.nextInt();
+
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) { // % 4 == 0 -> O ano é divisível por 4 && (e) % 100 != 0 -> O ano não é divisível por 100 || (ou) % 400 == 0 -> O ano é divisível por 400
+            System.out.println("O ano é bissexto.");
+
+        } else {
+            System.out.println("O ano não é bissexto.");
+        }
+
+    }
+
+}
