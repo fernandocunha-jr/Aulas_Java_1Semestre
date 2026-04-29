5dc08fd892f4d05123f3384c4508bedad61fc5b0
Fernando Cunha Júnior <laurenticunha@gmail.com>
2026-04-29 15:48:58 -0300

    Enviando novo algoritmo de Media e Soma utilizando Loop While

---
+++ aula6/Media_While.java
@@ -0,0 +1,29 @@
+package aula6;
+
+import java.util.Scanner;
+
+public class Media_While {
+    //Faça um algoritmo que pede para o usuário digitar números. O algoritmo deve calcular a média dos números e mostrar o resultado.
+    //Encerrar o programa quando o usuário digitar o número 0.
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double num = 1; // Inicializa a variável num com um valor diferente de 0 para entrar no loop.
+        double soma = 0; // Variável para acumular a soma dos números digitados   
+        int cont = 0; // Variável para contar a quantidade de números digitados
+        double media; // Variável para armazenar a média dos números digitados  
+
+        while (num != 0) { // O loop continua enquanto num for diferente de 0.
+
+            System.out.println("Digite um número (ou 0 para encerrar):");
+            num = sc.nextDouble(); // Lê o número digitado pelo usuário.
+            soma += num; // Adiciona o número digitado à soma total.
+            cont++; // Incrementa a contagem de números digitados.
+        }
+
+        media = soma / (cont - 1); // Calcula a média dos números digitados. Subtrai 1 de cont para não incluir o número 0 na contagem.
+        System.out.println("A média dos números digitados é: " + media); 
+    }
+}
---
+++ aula6/Soma_While.java
@@ -0,0 +1,28 @@
+package aula6;
+
+import java.util.Scanner;
+
+public class Soma_While {
+    //Faça um algoritmo que pede para o usuário digitar números inteiros positivos. O algoritmo deve somar os números e mostrar o resultado.
+    //Encerrar o programa quando o usuário digitar um número negativo.
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = 0;
+        int soma = 0;
+
+        while (num >= 0) {
+            soma += num; // soma = soma + num;
+
+            System.out.println("Digite um número inteiro positivo (ou um número negativo para encerrar):");
+            num = sc.nextInt();
+        }
+
+        System.out.println("A soma dos números digitados é: " + soma);
+    }
+
+
+
+}
