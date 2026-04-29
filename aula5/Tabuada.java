a972b6fe208e034ee7acfbaf9aa8b71f62901e42
Fernando Cunha Júnior <laurenticunha@gmail.com>
2026-04-29 15:14:36 -0300

    Atualizando a tabuada

--- aula5/Tabuada.java
+++ aula5/Tabuada.java
@@ -1,22 +1,14 @@
 package aula5;
 
-import java.util.Scanner;
-
 public class Tabuada {
-    // Faça um algoritmo que imprima a tabuada de um número informado pelo usuário de 1 a 10.
-
+    // Faça um algoritmo que imprima a tabuada completa de 1 a 10.
     public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
-
-            int num;
-
-            System.out.println("Digite um número para exibir a tabuada de 1 a 10:");
-            num = sc.nextInt();
-
-        for(int cont = 1; cont <= 10; cont++) { // cont = 1: comecar a tabuada do 1, cont <= 10: termina a tabuada até o 10.
+        for(int num = 1; num <= 10; num++) {
+            for(int cont = 1; cont <= 10; cont++) { // cont = 1: comecar a tabuada do 1, cont <= 10: termina a tabuada até o 10.
                                                 // cont++: faz adicionar +1 a cada repetição do loop.
             System.out.println(num + " x " + cont + " = " + (num * cont));
+            }
         }
     }
 }
