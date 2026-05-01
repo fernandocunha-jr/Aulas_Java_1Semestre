3a4c6e1eee0d8897d60bb1377d8225187e1ea038
Fernando Cunha Júnior <laurenticunha@gmail.com>
2026-04-30 22:44:01 -0300

    Enviando atualização do algoritmo de conversao
    Utilizando switch case

--- aula6/Conversao_Hexa.java
+++ aula6/Conversao_Hexa.java
@@ -2,7 +2,7 @@ package aula6;
 
 import java.util.Scanner;
 
-public class Hexa_Deci {
+public class Conversao_Hexa {
     //Faça um algoritmo que pede para o usuário digitar um número hexadecimal.
     //O algoritmo deve converter o número hexadecimal para decimal e mostrar o resultado.
 
@@ -10,33 +10,88 @@ public class Hexa_Deci {
 
         Scanner sc = new Scanner(System.in);
 
-        String hex;
+        String hex; // Variável para armazenar o número hexadecimal digitado pelo usuário.
         int decimal = 0;
 
         System.out.println("Digite um número hexadecimal:");
         hex = sc.nextLine().toUpperCase();// Lê o número hexadecimal digitado pelo usuário e converte para maiúsculas para facilitar a comparação.
 
         for(int i = 0; i < hex.length(); i++) {
-            String digito = hex.substring(i, i + 1); // Extrai o dígito hexadecimal da posição i.
+            String digito = hex.substring(i, i + 1); //Extrai o digito hexadecimal da posição i usando o método substring.
+                                    //+1 porque o substring pega o caractere da posição i até a posição i+1, ou seja, apenas um caractere.
 
-            int valor = 0; // Variável para armazenar o valor decimal correspondente ao dígito hexadecimal.
+            int valor;// Variável para armazenar o valor decimal correspondente ao dígito hexadecimal.
 
-            if (digito.equals("0")) valor = 0;
-            else if (digito.equals("1")) valor = 1;
-            else if (digito.equals("2")) valor = 2;
-            else if (digito.equals("3")) valor = 3;
-            else if (digito.equals("4")) valor = 4;
-            else if (digito.equals("5")) valor = 5;
-            else if (digito.equals("6")) valor = 6;
-            else if (digito.equals("7")) valor = 7;
-            else if (digito.equals("8")) valor = 8;
-            else if (digito.equals("9")) valor = 9;
-            else if (digito.equals("A")) valor = 10;
-            else if (digito.equals("B")) valor = 11;
-            else if (digito.equals("C")) valor = 12;
-            else if (digito.equals("D")) valor = 13;
-            else if (digito.equals("E")) valor = 14;
-            else if (digito.equals("F")) valor = 15;
+            switch (digito) {
+                case "0" -> {
+                     valor = 0;
+                     break;
+                }
+                case "1" -> {
+                    valor = 1;
+                    break;
+                }
+                case "2" -> {
+                    valor = 2;
+                    break;
+                }
+                case "3" -> {
+                    valor = 3;
+                    break;
+                }
+                case "4" -> {
+                    valor = 4;
+                    break;
+                }
+                case "5" -> {
+                    valor = 5;
+                    break;
+                }
+                case "6" -> {
+                    valor = 6;
+                    break;
+                }
+                case "7" -> {
+                    valor = 7;
+                    break;
+                }
+                case "8" -> {
+                    valor = 8;
+                    break;
+                }
+                case "9" -> {
+                    valor = 9;
+                    break;
+                }
+                case "A" -> {
+                    valor = 10;
+                    break;
+                }
+                case "B" -> {
+                    valor = 11;
+                    break;
+                }
+                case "C" -> {
+                    valor = 12;
+                    break;
+                }
+                case "D" -> {
+                    valor = 13;
+                    break;
+                }
+                case "E" -> {
+                    valor = 14;
+                    break;
+                }
+                case "F" -> {
+                    valor = 15;
+                    break;
+                }
+                default -> {
+                    System.out.println("Dígito hexadecimal inválido!");
+                    return;
+                }
+            }
 
             decimal = decimal * 16 + valor; // Converte o dígito hexadecimal para decimal e acumula o resultado.
         }
